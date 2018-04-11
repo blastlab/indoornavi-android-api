c1339e27e85a596bad604acb6893b27306e2f847
Agata Chmielewska <achmielewska@blastlab.co>
2018-04-11 10:54:56 +0200

    dodanie kilku zabezpieczeń WebView

--- indoornavi_api/src/main/java/co/blastlab/indoornavi_api/INMap.java
+++ indoornavi_api/src/main/java/co/blastlab/indoornavi_api/INMap.java
@@ -1,24 +1,20 @@
 package co.blastlab.indoornavi_api;
 
-import android.content.Context;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
+import android.content.Context;;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.webkit.ValueCallback;
+import android.webkit.ValueCallback;;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 import java.io.InputStream;
 
-import co.blastlab.indoornavi_api.callback.OnViewReadyCallback;
 import co.blastlab.indoornavi_api.web_view.IndoorWebChromeClient;
+import co.blastlab.indoornavi_api.web_view.IndoorWebViewClient;
 import co.blastlab.indoornavi_api.web_view.JavaScriptInterface;
 
 public class INMap extends WebView {
 
 	private JavaScriptInterface JSInterface;
-	OnViewReadyCallback mapReadyCallback;
 
 	private Context context;
 
@@ -29,7 +25,6 @@ public class INMap extends WebView {
 	public INMap(Context context, AttributeSet attributeSet){
 		super(context, attributeSet);
 		this.context = context;
-		mapReadyCallback = (OnViewReadyCallback) context;
 
 		loadWebViewFromAssets();
 		init();
@@ -44,27 +39,18 @@ public class INMap extends WebView {
 
 	private void init()
 	{
-
-		this.setWebViewClient(new WebViewClient() {
-
-			@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-			@Override
-			public void onPageFinished(WebView view, String url) {
-				super.onPageFinished(view, url);
-
-				mapReadyCallback.onWebViewReady((INMap) view);
-			}
-
-		});
-
+		this.setWebViewClient(new IndoorWebViewClient());
 		this.setWebChromeClient(new IndoorWebChromeClient());
 
 		this.getSettings().setJavaScriptEnabled(true);
 		this.getSettings().setDomStorageEnabled(true);
 		//this.getSettings().setUseWideViewPort(true);
 		this.getSettings().setLoadWithOverviewMode(true);
-		this.getSettings().setDomStorageEnabled(true);
 
+		this.getSettings().setAllowFileAccess(false);
+		this.getSettings().setAllowFileAccessFromFileURLs(false);
+		this.getSettings().setAllowUniversalAccessFromFileURLs(false);
+		this.getSettings().setAllowContentAccess(false);
 	}
 
 	public void createMap(String targetHost, String apiKey)
